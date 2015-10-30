b8c3e8d2adf4243a03d3e5fef774a193e483bba5
saad689 <saad689@gmail.com>
2015-10-29 23:16:29 -0400

    10/29/2015 Feature Selection Submission

---
+++ ud120-projects-master/feature_selection/find_signature.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python
+
+import pickle
+import numpy
+numpy.random.seed(42)
+from sklearn import tree
+from sklearn.metrics import accuracy_score
+
+
+### The words (features) and authors (labels), already largely processed.
+### These files should have been created from the previous (Lesson 10)
+### mini-project.
+words_file = "../text_learning/your_word_data.pkl" 
+authors_file = "../text_learning/your_email_authors.pkl"
+word_data = pickle.load( open(words_file, "r"))
+authors = pickle.load( open(authors_file, "r") )
+
+
+
+### test_size is the percentage of events assigned to the test set (the
+### remainder go into training)
+### feature matrices changed to dense representations for compatibility with
+### classifier functions in versions 0.15.2 and earlier
+from sklearn import cross_validation
+features_train, features_test, labels_train, labels_test = cross_validation.train_test_split(word_data, authors, test_size=0.1, random_state=42)
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+vectorizer = TfidfVectorizer(sublinear_tf=True, max_df=0.5,
+                             stop_words='english')
+features_train = vectorizer.fit_transform(features_train)
+features_test  = vectorizer.transform(features_test).toarray()
+
+
+### a classic way to overfit is to use a small number
+### of data points and a large number of features;
+### train on only 150 events to put ourselves in this regime
+features_train = features_train[:150].toarray()
+labels_train   = labels_train[:150]
+
+
+
+### your code goes here
+clf_dt = tree.DecisionTreeClassifier()
+clf_dt = clf_dt.fit(features_train, labels_train)
+pred = clf_dt.predict(features_test)
+print "The accuracy of the DT Classifier is: ", accuracy_score(labels_test, pred)
+print "The number of training points = ", len(features_train)
+
+print "Highest importance: ", [x for x in clf_dt.feature_importances_ if x > 0.2]
+
+idx = [x for x in range(len(clf_dt.feature_importances_)) if clf_dt.feature_importances_[x] > 0.2]
+print "Index of highest importance: ", idx
+
+list_words = vectorizer.get_feature_names()
+print list_words[idx[0]]
