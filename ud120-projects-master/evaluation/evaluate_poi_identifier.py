f7497adb98dd6f386dacc0b4445c6c9511853957
saad689 <saad689@gmail.com>
2015-11-09 19:22:18 -0500

    11/09/2015 Evaluate mini project submission

---
+++ ud120-projects-master/evaluation/evaluate_poi_identifier.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python
+
+
+"""
+    Starter code for the evaluation mini-project.
+    Start by copying your trained/tested POI identifier from
+    that which you built in the validation mini-project.
+
+    This is the second step toward building your POI identifier!
+
+    Start by loading/formatting the data...
+"""
+
+import pickle
+import sys
+sys.path.append("../tools/")
+from feature_format import featureFormat, targetFeatureSplit
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score, precision_score, recall_score
+from sklearn import cross_validation
+import math
+
+data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "r") )
+
+### add more features to features_list!
+features_list = ["poi", "salary"]
+
+data = featureFormat(data_dict, features_list)
+labels, features = targetFeatureSplit(data)
+
+
+
+### your code goes here 
+features_train, features_test, labels_train, labels_test = cross_validation.train_test_split(features, labels, test_size=0.3, random_state=42)
+
+clf = DecisionTreeClassifier()
+clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
+print accuracy_score(pred, labels_test)
+print "If all the predictions were 0 the accuracy would be: ", accuracy_score([min(i,0.) for i in pred], labels_test)
+
+true_pos = 0
+for x, y in zip(pred, labels_test):
+    if x == 1 and x == y:
+        true_pos = true_pos + 1
+        
+print "Number of true positives: ", true_pos
+
+print "Precision score: ", precision_score(labels_test, pred)
+print "Recall score: ", recall_score(labels_test, pred)
+
+predictions = [0, 1, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0, 1, 0, 1] 
+true_labels = [0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 0]
+
